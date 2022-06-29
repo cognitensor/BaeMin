d9ce325dc7fb9ba679302f9ec5f79464b2553bff
HANLeeeee <hanleeeee@naver.com>
2022-06-30 01:01:32 +0900

    Initial Commit

---
+++ baminTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  baminTest
+//
+//  Created by 하늘이 on 2022/06/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
